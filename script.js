42f8647105aee1aa8ebcb811d4e46f0594eafb2b
steezrogers <stevelee83@gmail.com>
2022-07-31 19:56:35 -0700

    Add loop function game for playRound

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// create function getComputerChoice that randomly chooses
-// from 'Rock', 'Paper', and 'Scissors'. Return result in console
+// create function getComputerChoice() that randomly chooses
+// from 'Rock', 'Paper', and 'Scissors'
 
 const choices = ['Rock', 'Paper', 'Scissors']
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-// create function playRound (playerSelection, computerSelection)
+// create function playRound(playerSelection, computerSelection)
 // that returns a string based on result. make playerSelection case-insensitive
 
 function playRound(playerSelection, computerSelection) {
@@ -17,8 +17,21 @@ function playRound(playerSelection, computerSelection) {
     } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') ||
         (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') ||
         (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock')) {
-        alert(`${computerSelection} beats ${playerSelection}. YOU LOSE T_T`);    
+        alert(`${playerSelection} loses to ${computerSelection}. LOSER!`);    
     } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         alert('Tie. Try Again.');
     }
 }
+
+// create function game() that loops playRound() 5 times. add feature that 
+// keeps track of score and reports a winner or loser after 5 rounds
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Please choose "rock", "paper", or "scissors".');
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+game();
