043ef3e5aa6d9ea37edb2a41665818703bd0dae4
steezrogers <stevelee83@gmail.com>
2022-07-31 20:47:52 -0700

    Add score vars and code to keep track of score

--- script.js
+++ script.js
@@ -8,18 +8,26 @@ function getComputerChoice() {
 
 // create function playRound(playerSelection, computerSelection)
 // that returns a string based on result. make playerSelection case-insensitive
+// add playerScore and computerScore variables to keep track of score
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') ||
         (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') ||
         (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper')) {
-        alert(`${playerSelection} beats ${computerSelection}. YOU WIN!`);         
+        alert(`${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()} beats ${computerSelection}. Nice.`); 
+        playerScore += 1;        
     } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') ||
         (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') ||
         (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock')) {
-        alert(`${playerSelection} loses to ${computerSelection}. LOSER!`);    
+        alert(`${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()} loses to ${computerSelection}. Not nice.`);
+        computerScore += 1;    
     } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        alert('Tie. Try Again.');
+        alert('Tie. Nice?');
+    } else {
+        alert('Please enter a proper value.')
     }
 }
 
@@ -31,6 +39,13 @@ function game() {
         let playerSelection = prompt('Please choose "rock", "paper", or "scissors".');
         let computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
+    }
+    if (playerScore > computerScore) {
+        alert(`Winner Winner Chicken Dinner! Player: ${playerScore} Computer: ${computerScore}`);
+    } else if (playerScore < computerScore) {
+        alert(`YOU DIED. Player: ${playerScore} Computer: ${computerScore}`)
+    } else if (playerScore == computerScore) {
+        alert(`There are no ties in Jan-Ken-Pon. Try again. Player: ${playerScore} Computer: ${computerScore}`);
     }
 }
 
