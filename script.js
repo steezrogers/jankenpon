62103b8b4b7b2275c853aac79664cc3bc48903d7
steezrogers <stevelee83@gmail.com>
2022-07-31 18:08:19 -0700

    Create playRound function and update code
    Updated code and comments for function getComputerChoice

--- script.js
+++ script.js
@@ -2,9 +2,23 @@
 // from 'Rock', 'Paper', and 'Scissors'. Return result in console
 
 const choices = ['Rock', 'Paper', 'Scissors']
-function getComputerChoice(choices) {
+function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-console.log(getComputerChoice(choices));
+// create function playRound (playerSelection, computerSelection)
+// that returns a string based on result. make playerSelection case-insensitive
 
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') ||
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper')) {
+        alert(`${playerSelection} beats ${computerSelection}. YOU WIN!`);         
+    } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') ||
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock')) {
+        alert(`${computerSelection} beats ${playerSelection}. YOU LOSE T_T`);    
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        alert('Tie. Try Again.');
+    }
+}
